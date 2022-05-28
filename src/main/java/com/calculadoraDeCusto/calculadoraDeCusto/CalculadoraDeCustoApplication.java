827b90b2d1128116dd35f1441b947e441fe132e6
Moises Pompilio <moisesjtmed@gmail.com>
2022-05-28 17:40:01 -0300

    first commit

---
+++ src/main/java/com/calculadoraDeCusto/calculadoraDeCusto/CalculadoraDeCustoApplication.java
@@ -0,0 +1,13 @@
+package com.calculadoraDeCusto.calculadoraDeCusto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraDeCustoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraDeCustoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calculadoraDeCusto/calculadoraDeCusto/CalculadoraDeCustoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculadoraDeCusto.calculadoraDeCusto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraDeCustoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
